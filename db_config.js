9ee6978bb762366fd7e79f4602869c9aaacecd3b
Tharangani Hagoda Arachchi <tharangani@Tharanganis-MacBook-Air.local>
2025-01-01 23:56:41 +0530

    modification

--- db_config.js
+++ db_config.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const DBConnect = async () => {
     try{
-        await mongoose.connect(process.env.MONGO_URL,);
+        await mongoose.connect(process.env.MONGO_URI,);
         console.log('MongoDB connected')
 
     }catch(error)
